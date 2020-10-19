2c49d0f1c6e29d8b4f6da131988e8c84798fc3be
yooperjb <jmb181@humboldt.edu>
2020-10-19 14:24:32 -0700

    Fetch request for github function

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,6 +1,16 @@
 
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
-};
+var getUserRepos = function(user) {
+    // format the gihub api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
-getUserRepos();
+    // make a request to the url
+    fetch(apiUrl).then(function(response){
+        response.json().then(function(data){
+            console.log(data);
+        });
+    });
+};
+// asynchoronous behaviour - will print before inside
+// AJAX Asynchronous javascript and XML
+console.log("outside");
+getUserRepos("microsoft");
