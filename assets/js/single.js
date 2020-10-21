d3ffea1be9c825ff492054a181c8f1da0ca0d517
Pedal <jmb181@humboldt.edu>
2020-10-20 18:29:11 -0700

    Added warning if repo has more than 30 issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,6 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
+
 
 var getRepoIssues = function(repo) {
     console.log(repo);
@@ -11,6 +13,11 @@ var getRepoIssues = function(repo) {
                 .then(function(data){
                     // pass response data to dom function
                     displayIssues(data);
+
+                    // check if api has paginated issues (more than 30)
+                    if (response.headers.get("Link")) {
+                        displayWarning(repo);
+                    }
                 });
         }
         else {
@@ -57,6 +64,18 @@ var displayIssues = function(issues) {
         // append to html page - issueContainerEl
         issueContainerEl.appendChild(issueEl);
     }
+};
+
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent - "To see more than 30 issues, visit ";
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
 };
 
 getRepoIssues("facebook/react");
